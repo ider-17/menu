bffef818c1f90ed47ac668517e2f8eda01cc675d
ider <ider@iders-MacBook-Pro.local>
2025-03-06 22:22:35 +0800

    up

--- src/app/page.js
+++ src/app/page.js
@@ -99,3 +99,37 @@ export default function Home() {
     </div>
   );
 }
+// const menu = [
+//   {
+//     category: "SALADS",
+//     items: [
+//       { name: "CHICKEN CAESAR SALAD", tailbar: "Served with Sundried tomatoes, chunky garlic croutons and grilled Chicken", price: "24,900₮" },
+//       { name: "GREEK SALAD", tailbar: "Fresh cucumbers, tomatoes, red onions, Kalamata olives, and feta cheese, drizzled with olive oil.", price: "21,900₮" },
+//       { name: "QUINOA POWER SALAD", tailbar: "A healthy mix of quinoa, avocado, roasted sweet potatoes, and kale with a tangy lemon dressing.", price: "22,500₮" },
+//     ],
+//   },
+//   {
+//     category: "BURGERS",
+//     items: [
+//       { name: "CLASSIC BURGER", tailbar: "Juicy beef patty, cheddar cheese, lettuce, tomato, and house-made sauce, served with fries.", price: "18,900₮" },
+//       { name: "BBQ BACON BURGER", tailbar: "Beef patty topped with crispy bacon, BBQ sauce, cheddar, and onion rings.", price: "21,900₮" },
+//       { name: "VEGAN BLACK BEAN BURGER", tailbar: "Homemade black bean patty, avocado, lettuce, tomato, and vegan mayo, served on a whole-grain bun.", price: "17,900₮" },
+//     ],
+//   },
+//   {
+//     category: "PASTA",
+//     items: [
+//       { name: "SPAGHETTI PASTA", tailbar: "Classic Italian pasta with creamy egg sauce, Parmesan cheese, and crispy pancetta.", price: "20,900₮" },
+//       { name: "SEAFOOD MARINARA", tailbar: "Fresh cucumbers, tomatoes, red onions, Kalamata olives, and feta cheese, drizzled with olive oil.", price: "23,500₮" },
+//       { name: "PESTO CHICKEN PASTA", tailbar: "Fresh cucumbers, tomatoes, red onions, Kalamata olives, and feta cheese, drizzled with olive oil.", price: "21,000₮" },
+//     ],
+//   },
+//   {
+//     category: "PIZZA",
+//     items: [
+//       { name: "MARGHERITA PIZZA", tailbar: "Simple and classic with tomato sauce, fresh mozzarella, and basil leaves.", price: "30,900₮" },
+//       { name: "PEPPERONI FEAST", tailbar: "Loaded with pepperoni and melted mozzarella on a crispy crust.", price: "34,500₮" },
+//       { name: "VEGETARIAN DELIGHT", tailbar: "Topped with bell peppers, mushrooms, olives, onions, and fresh tomatoes.", price: "29,900₮" },
+//     ],
+//   },
+// ];
